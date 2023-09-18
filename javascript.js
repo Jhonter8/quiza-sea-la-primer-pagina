32685f22f99dea25158d19a1c08da7532c815c98
Jhonter <rosasJonathan1@hotmail.com>
2023-09-18 18:05:54 -0300

    se actualiza el javascript

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 let nombre = prompt("hola, tu nombre?");
 
-let saludar = alert(`hola ${nombre} un gusto soy jhonter`);
+let saludar = alert(`hola ${nombre} un gusto soy jhonter y esto es un nuevo commit`);
 
